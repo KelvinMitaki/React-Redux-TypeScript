1646abeb956931aa3e0198377947cb1f2c30ab47
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-11 17:51:49 +0300

    added the todo interface on axios fetch todos

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,10 +1,16 @@
 import Axios from "axios";
 import { Dispatch } from "redux";
 
-const URL = "https://www.jsonplaceholder.typicode.com/todos";
+const URL = "https://jsonplaceholder.typicode.com/todos";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const fetchTodos = () => async (dispatch: Dispatch) => {
-  const res = await Axios.get(URL);
+  const res = await Axios.get<Todo[]>(URL);
   dispatch({
     type: "FETCH_TODOS",
     payload: res.data
