583baa6dc31eb1b9c5c1229af428dd5e7558eda5
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-11 19:34:30 +0300

    wired up deleting todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchTodos, Todo } from "./redux/actions";
+import { deleteTodo, fetchTodos, Todo } from "./redux/actions";
 import { StoreState } from "./redux/reducers";
 
 interface AppProps {
   todos: Todo[];
   fetchTodos(): any;
+  deleteTodo(id: number): any;
 }
 
 class App extends Component<AppProps> {
@@ -15,7 +16,10 @@ class App extends Component<AppProps> {
         <button onClick={(): void => this.props.fetchTodos()}>Fetch</button>
         {this.props.todos.map(
           (todo: Todo): JSX.Element => (
-            <div key={todo.id}>
+            <div
+              onClick={(): void => this.props.deleteTodo(todo.id)}
+              key={todo.id}
+            >
               <div>{todo.title}</div>
             </div>
           )
@@ -29,4 +33,4 @@ const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
     todos: state.todos
   };
 };
-export default connect(mapStateToProps, { fetchTodos })(App);
+export default connect(mapStateToProps, { fetchTodos, deleteTodo })(App);
--- src/redux/reducers/todosReducer.ts
+++ src/redux/reducers/todosReducer.ts
@@ -1,10 +1,14 @@
-import { FetchTodosAction, Todo } from "../actions";
+import { DeleteTodoAction, FetchTodosAction, Todo } from "../actions";
 import { ActionTypes } from "../actions/types";
 
-const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+type Action = FetchTodosAction | DeleteTodoAction;
+
+const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo): boolean => todo.id !== action.payload);
     default:
       return state;
   }
